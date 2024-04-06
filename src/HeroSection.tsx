a27247c964e20d7d6065bb90790966fb2ec95118
José Ângelo da Silva Nery <josenerydev@gmail.com>
2024-04-06 11:58:17 -0300

    Update HeroSection title and subtitle

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -11,8 +11,10 @@ const HeroSection: React.FC<Props> = (props) => {
     <section className="hero is-primary is-bold">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">Bem-vindo ao Exemplo com Bulma</h1>
-          <h2 className="subtitle">Um simples modelo para começar</h2>
+          <h1 className="title">Bem-vindo ao Eximia Story Builder</h1>
+          <h2 className="subtitle">
+            A ferramenta definitiva para transformar ideias em User Stories concisas e impactantes. Agilize seu planejamento e fomente a colaboração com histórias que orientam o desenvolvimento focado no usuário.
+          </h2>
         </div>
       </div>
     </section>
